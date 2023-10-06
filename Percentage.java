a2ad55ae5dd1ec03a60f475c498b9e70c5b4f882
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-06 23:00:00 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,13 @@
+public class Percentage {
+    public static void main(String[] args) {
+        double a=90;
+        double b=91;
+        double c=92;
+        double d=93;
+        double e=94;
+
+        double ans=(a+b+c+d+e)/5;
+
+        System.out.println(ans);
+    }
+}
