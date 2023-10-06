843522def21cb82e79b652af8deb79c70236872f
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-06 22:57:27 +0530

    Add files via upload

---
+++ Area_Of_Circle.java
@@ -0,0 +1,10 @@
+public class Area_Of_Circle {
+    public static void main(String[] args) {
+        double radius=5;
+        double pi=3.1415;
+
+        double area=pi*radius*radius;
+
+        System.out.println(area);
+    }
+}
