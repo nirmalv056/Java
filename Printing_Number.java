17e18dc56b07821c977e05ca193f958e1c438293
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-09 20:45:48 +0530

    Add files via upload

---
+++ Printing_Number.java
@@ -0,0 +1,8 @@
+public class Printing_Number {
+    public static void main(String[] args) {
+        System.out.println("4");
+        System.out.println(4);
+        System.out.println("4+4");
+        System.out.println(4+4);
+    }
+}
