81b462e3405d55fb37214ca32d5f49f8857defd3
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-09 20:46:25 +0530

    Add files via upload

---
+++ Simple_Interest.java
@@ -0,0 +1,14 @@
+public class Simple_Interest {
+    public static void main(String[] args) {
+
+        double p,r,t,si;
+        p=1000;
+        r=5;
+        t=2;
+
+        si=(p*r*t)/100;
+
+        System.out.println(si);
+    }
+
+}
