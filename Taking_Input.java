16995266a580a2b5e91919fd9d20458ce72cdfbd
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-26 23:24:30 +0530

    Add files via upload

---
+++ Taking_Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Taking_Input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your lucky Number: ");
+        int num1=sc.nextInt();
+
+        System.out.println("Your lucky number is: "+num1);
+    }
+}
