82a8a819b5d59bc932627b1203af93ab04937147
Nirmal Kumar Verma <128505766+nirmalv056@users.noreply.github.com>
2023-10-26 23:24:51 +0530

    Add files via upload

---
+++ Variables_Int.java
@@ -0,0 +1,11 @@
+public class Variables_Int {
+    public static void main(String[] args) {
+        int x;
+        x=500;
+        System.out.println(x);
+        x=x+5;
+        System.out.println(x);
+        x=x-10;
+        System.out.println(x);
+    }
+}
